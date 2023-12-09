60ebd54201305a1863607533dc8eac0681f0397d
Dpetra <146233888+Dpetra@users.noreply.github.com>
2023-12-09 19:37:46 +0100

    Feladatok megoldása

--- A3U8XV_12.05/A3U8XV_8.gyak2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-favourite_number=7
-echo "Kedvenc számom: $favourite_number"
-exit 0
--- A3U8XV_12.05/A3U8XV_8.gyakon1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Felhasználónév: " nev
-read -p "Verzió: " verzio
-read -p "Webhely: " webhely
-json_adat="{Username: $nev\nVersion: $verzio\nSite: $webhely}"
-echo $json_adat > config.yml
-exit 0
